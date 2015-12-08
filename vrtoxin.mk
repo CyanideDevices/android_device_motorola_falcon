a5e8186958be09022a83445ca3ea4d9ecf93063d
srisurya95 <srisurya95@gmail.com>
2015-12-08 02:51:52 +0000

    Go Toxic
    Conflicts:
    	vrtoxin.mk

---
+++ vrtoxin.mk
@@ -0,0 +1,10 @@
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/vrtoxin/config/common_full_phone.mk)
+
+# Inherit device specific
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := vrtoxin_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
